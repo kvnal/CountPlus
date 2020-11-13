186ab4e32378ed28e790f18f261ab54f87e65f39
kvnal <72142540+kvnal@users.noreply.github.com>
2020-11-13 16:01:54 +0530

    initial commit⌚
    initial commit (basic features)

--- app/src/main/java/com/example/count/MainActivity.kt
+++ app/src/main/java/com/example/count/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val TAG="hello"
 
-        var count=990
+        var count=0
 
         layout.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
